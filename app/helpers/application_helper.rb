ce97d43bc6db33c66367144f8d46fee77aed1093
Dustin Rodrigues <dust.rod@gmail.com>
2011-07-26 16:48:06 -0400

    changed layout/font

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     '<script type="text/javascript">
 
   var _gaq = _gaq || [];
-  _gaq.push([\'_setAccount\', \'UA-22106919-1\']);
+  _gaq.push([\'_setAccount\', \'UA-\']);
   _gaq.push([\'_trackPageview\']);
 
   (function() {
@@ -22,7 +22,7 @@ module ApplicationHelper
 
   def clicky
     '<script src="http://static.getclicky.com/js" type="text/javascript"></script>
-    <script type="text/javascript">try{ clicky.init(66400367); }catch(err){}</script>'
+    <script type="text/javascript">try{ clicky.init(); }catch(err){}</script>'
   end
 
 end
