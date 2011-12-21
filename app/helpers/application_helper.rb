d9f4f28ca8ee7a7bab016dedda2c8e9af80c12e2
Dustin Rodrigues <dust.rod@gmail.com>
2011-12-21 15:55:15 -0800

    added clicky init value

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,7 +22,7 @@ module ApplicationHelper
 
   def clicky
     '<script src="http://static.getclicky.com/js" type="text/javascript"></script>
-    <script type="text/javascript">try{ clicky.init(); }catch(err){}</script>'
+    <script type="text/javascript">try{ clicky.init(66457842); }catch(err){}</script>'
   end
 
 end
