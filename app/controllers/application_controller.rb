28ef8e7ef2d75095655f4019f46ce5d2acd431bc
Eugene Aarons-Cooke <saauron@gmail.com>
2012-03-28 17:55:48 -0700

    removed new window blog link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+   def index
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    render :text => "Rendered at #{Time.now}"
+  end
+  
 end
